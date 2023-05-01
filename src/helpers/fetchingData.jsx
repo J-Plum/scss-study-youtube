674e809bb2c93af87179cd9eaf17066724924e50
J-Plum <bosswjdwo@gmail.com>
2023-05-01 20:26:41 +0900

    axios 설정 및 fetch 가공

---
+++ src/helpers/fetchingData.jsx
@@ -0,0 +1,39 @@
+import axios from "../api/axios";
+
+export const getVideoInfo = async (videoArray) => {
+  try {
+    for (let video of videoArray) {
+      console.log(video.id.videoId);
+      const videoResponse = await axios.get(
+        `/videos?part=snippet&part=contentDetails&part=player&part=statistics&id=${video.id.videoId}`
+      );
+      Object.assign(video.snippet, { ...videoResponse.data.items[0].snippet });
+      video.extraInfo = Object.assign(
+        {},
+        videoResponse.data.items[0].tags,
+        videoResponse.data.items[0].contentDetails,
+        videoResponse.data.items[0].statistics,
+        videoResponse.data.items[0].player
+      );
+
+      // 체널 정보
+      const channelResponse = await axios.get(
+        `/channels?part=snippet&part=statistics&part=contentDetails&id=${video.snippet.channelId}`
+      );
+
+      const channelResultA = channelResponse.data.items[0].snippet;
+      const channelResultB = channelResponse.data.items[0].statistics;
+      const channelInfo = Object.assign(
+        {},
+        {
+          ...channelResultA,
+          ...channelResultB,
+        }
+      );
+      video.channelInfo = channelInfo;
+    }
+    return videoArray;
+  } catch (error) {
+    console.error(error);
+  }
+};
--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,5 +1,24 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import axios from "../../api/axios";
+import { getVideoInfo } from "../../helpers/fetchingData";
 
-export default function index() {
-  return <div>MainPage</div>;
+export default function MainPage() {
+  const [mainVideos, setMainVideos] = useState([]);
+
+  const getMainVideos = useCallback(async () => {
+    try {
+      const response = await axios.get(`/search?part=snippet&maxResults=2&q='르세라핌'%20place`);
+      let videosArray = await response.data.items;
+      videosArray = await getVideoInfo(videosArray);
+      setMainVideos(videosArray);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    getMainVideos();
+  }, []);
+
+  return <section className="mainGallery"></section>;
 }
